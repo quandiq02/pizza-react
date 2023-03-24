dd20b06a9e3ce9e69bf412e4297cb8ef2523efcd
Quandiq <112625513+quandiq02@users.noreply.github.com>
2023-03-24 19:28:27 +0500

    Add files via upload

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__title">Build your pizza</div>
+          <div className="header__ingridients">Ingridients</div>
+        </div>
+      </div>
+    </header>
+  );
+}
